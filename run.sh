dc402c8d671548e3a21a2e414787432d039b0ba0
Noah Evans <nevans@sandia.gov>
2018-06-21 10:02:28 -0600

    add sbatch header

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+#SBATCH --nodes=2
+#SBATCH --ntasks=128
+#SBATCH --partition=knl
 
 echo rank iter size bufsize noise time
 for i in `awk 'BEGIN {for(i = 1; i < 2**16; i = i*2) print i}'`; do
-	srun -p knl -N 2 -n 128 ./a.out -n $i
+	srun ./a.out -n $i
+	sleep 10
 done

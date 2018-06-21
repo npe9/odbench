03623837e7813eadd6e4831874399671441ec91a
Noah Evans <noah.evans@gmail.com>
2018-06-21 11:28:00 -0600

    add barrier

--- main.c
+++ main.c
@@ -137,6 +137,7 @@ main(int argc, char **argv)
         printf("rank %d starting recv from %d\n", rank, rank - size/2);
       MPI_Recv(buf, bufsize, MPI_FLOAT, rank - size/2, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
+      MPI_Barrier(MPI_COMM_WORLD);
       finish = rdtscp(&cpuid);
       if(oldcpuid == cpuid)
         printf("%d %d comm %d %d %d %llu\n", rank, i, size, bufsize, noise, finish-start);
